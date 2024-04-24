9c9c87256833bd980b5b3291b1405b588e00704b
AntonioJesusRM <antjrm95@gmail.com>
2024-04-24 19:48:35 +0200

    Add interface Shape and the 3 class

--- app/src/main/java/com/example/sprint2lab/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/sprint2lab/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.sprint2lab
+package com.example.sprint2lab.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.sprint2lab.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
